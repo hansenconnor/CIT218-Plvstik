586a9fbfba67b4e3a1c892da62477543a11287ec
Connor Hansen <connorhansen@github.com>
2019-01-29 14:12:56 -0500

    first commit

---
+++ PLVSTIK/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using PLVSTIK.Models;
+
+namespace PLVSTIK.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<Product> prodList = new List<Product>
+            {
+                new Product
+                {
+                    Id = 0,
+                    Title = "Hand Bag",
+                    Description = "Classy handbag to take with you anywhere.",
+                    Price = 50.00,
+                },
+            };
+
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Contact")]
+        public ActionResult Contact(string name)
+        {
+            ViewBag.Message = "Thank you for contacting us, " + name + "!";
+            return View("ContactFormSuccess");
+        }
+
+        [Route("Contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("ContactFormSuccess")]
+        public ActionResult ContactFormSuccess()
+        {
+            return View();
+        }
+
+        //public ActionResult Shop(int? productId)
+        //{
+        //Product product = new Product();
+
+        //foreach (Product product in products.ProdList)
+        //{
+        //    if (product.Id == productId)
+        //    {
+        //        ViewBag.productId.ToString();
+        //    }
+        //}
+        //return View(myProduct);
+        //}
+
+    }
+}
---
+++ PLVSTIK/Controllers/ShopController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PLVSTIK.Models;
+
+namespace PLVSTIK.Controllers
+{
+    public class ShopController : Controller
+    {
+        // Create a list of products
+        List<Product> products = new List<Product>
+        {
+            new Product
+            {
+                Id = 1,
+                Title = "PLVSTIK Purse",
+                Description = "Limited edition PLVSTIK purse. Availabile in pink or blue.",
+                Price = 50.00,
+                ImageUrl = "https://images.unsplash.com/photo-1524672353063-4f66ee1f385e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1349&q=80",
+            },
+            new Product
+            {
+                Id = 2,
+                Title = "PLVSTIK Shirt",
+                Description = "Custom PLVSTIK shirt featuring a skeleton hand graphic.",
+                Price = 20.00,
+                ImageUrl = "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+            },
+            new Product
+            {
+                Id = 3,
+                Title = "PLVSTIK Sunglasses",
+                Description = "PLVSTIK x RayBan sunglasses with a classic green tint.",
+                Price = 150.00,
+                ImageUrl = "https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+            },
+        };
+
+        [HttpPost]
+        public ActionResult Index(string name) // Pre-Order success view/message
+        {
+            ViewBag.Message = "Thank you for your pre-order, " + name + "!";
+            return View("PreOrderSuccess");
+        }
+
+        public ActionResult Index()
+        {
+            return View (products); // Display all products
+        }
+
+        public ActionResult PreOrderSuccess()
+        {
+            return View(); // Pre-Order success view/message
+        }
+
+        public ActionResult ViewProduct(int productId) // Product single view
+        {
+            foreach (Product product in products)
+            {
+                if (product.Id == productId)
+                {
+                    return View(product);
+                }
+            }
+            return View();
+        }
+    }
+}
