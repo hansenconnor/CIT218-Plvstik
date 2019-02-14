c8a36684111b4111715dd9bb77a5315a1080128e
Connor Hansen <hanse174@ad.nmc.edu>
2019-02-14 13:42:18 -0500

    Setting up db connection

---
+++ PLVSTIK/DAL/ProductContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using PLVSTIK.Models;
+
+namespace PLVSTIK.DAL
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext() : base("ProductContext")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ PLVSTIK/Models/ProductCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PLVSTIK.Models
+{
+    public enum Category
+    {
+        Bag, Shirt, Sweatshirt, Backpack, Glasses
+    }
+
+    public class ProductCategory
+    {
+        public int ID { get; set; }
+    }
+}
